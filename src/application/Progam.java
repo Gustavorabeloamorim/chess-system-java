ad66a464a36aeeaeb4b0fab85a7ca6169dfa6979
Gustavorabeloamorim <gustavo.amorim09@hotmail.com>
2024-05-03 20:43:31 -0300

    Placing pieces on the board

--- src/application/Progam.java
+++ src/application/Progam.java
@@ -5,13 +5,10 @@ import chess.ChessMatch;
 public class Progam {
 
 	public static void main(String[] args) {
-	
+
 		ChessMatch chessmatch = new ChessMatch();
 		UI.printBoard(chessmatch.getPieces());
-		
-		
-		
-		
+
 	}
 
 }
