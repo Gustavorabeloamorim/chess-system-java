4b44eeea407378dc990a0d15873dfbf3db491321
Gustavorabeloamorim <gustavo.amorim09@hotmail.com>
2024-05-06 15:31:20 -0300

    Upgrade Progam Exception

--- src/application/Progam.java
+++ src/application/Progam.java
@@ -1,14 +1,36 @@
 package application;
 
+import java.util.Scanner;
+
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Progam {
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
 
-		ChessMatch chessmatch = new ChessMatch();
-		UI.printBoard(chessmatch.getPieces());
-
+		ChessMatch chessMatch = new ChessMatch();
+		
+		
+		
+		
+		while(true) {
+		UI.printBoard(chessMatch.getPieces());
+		System.out.println();
+		System.out.print("Source: ");
+		ChessPosition source = UI.readChessPosition(sc);
+		
+		System.out.println();
+		System.out.print("Target: ");
+		ChessPosition target = UI.readChessPosition(sc);
+		
+		ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+		
+		
+		}
 	}
 
 }
